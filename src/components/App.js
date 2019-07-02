b1e6fe9c54a0f08e7bda2124825b434ac7dedcd6
Ruben Costa <daggosso2@gmail.com>
2019-07-02 18:26:09 +0200

    Add palette component and absolute imports

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Palette from "components/Palette";
+
+function App() {
+  return (
+    <div >
+      <h1>Test</h1>
+      <Palette/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Palette/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class index extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Palette Component</h1>
+            </div>
+        )
+    }
+}
