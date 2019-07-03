20bd8289838e0da7e20af1e69444c0739da5a326
Ruben Costa <daggosso2@gmail.com>
2019-07-03 02:32:43 +0200

    Add mode button and styles

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,7 +2,36 @@ import React, { Component } from "react";
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
 import "./style.css";
-import { white } from "ansi-colors";
+import Select from "react-select";
+
+const options = [
+    { value: "hex", label: "HEX" },
+    { value: "rgb", label: "RGB" },
+    { value: "rgba", label: "RGBA" }
+];
+
+const style = {
+    singleValue: base => ({
+        ...base,
+        color: "#fff"
+    }),
+    input: base => ({
+        ...base,
+        color: "#fff"
+    }),
+    control: (base, state) => ({
+        ...base, // 021b35
+        background: "#021b35",
+        borderRadius: "20px",
+        color: "#fff",
+        border: state.isFocused ? 0 : 0,
+        // This line disable the blue border
+        boxShadow: state.isFocused ? 0 : 0,
+        "&:hover": {
+            border: state.isFocused ? 0 : 0
+        }
+    })
+};
 
 export default class Navbar extends Component {
     render() {
@@ -19,17 +48,36 @@ export default class Navbar extends Component {
                         </span>
                     </a>
                 </div>
-                <div className="Navbar__slider__container">
-                    <span className="Navbar__slider__detail">
-                        Level: {this.props.level}
-                    </span>
-                    <div className="Navbar__slider">
-                        <Slider
-                            defaultValue={this.props.level}
-                            min={100}
-                            max={900}
-                            step={100}
-                            onAfterChange={this.props.changeLevel}
+                <div className="Navbar__container">
+                    <div className="Navbar__slider__container">
+                        <span className="Navbar__slider__detail">
+                            Level: {this.props.level}
+                        </span>
+                        <div className="Navbar__slider">
+                            <Slider
+                                defaultValue={this.props.level}
+                                min={100}
+                                max={900}
+                                step={100}
+                                onAfterChange={this.props.changeLevel}
+                            />
+                        </div>
+                    </div>
+                    <div className="Navbar__select">
+                        <Select
+                            defaultValue={{ value: "HEX", label: "HEX" }}
+                            options={options}
+                            styles={style}
+                            theme={theme => ({
+                                ...theme,
+                                borderRadius: 0,
+                                colors: {
+                                    ...theme.colors,
+                                    primary25: "#eeeeee",
+                                    primary: "#021b35",
+                                    color: "white"
+                                }
+                            })}
                         />
                     </div>
                 </div>
