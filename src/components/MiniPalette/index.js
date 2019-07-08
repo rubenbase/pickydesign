d5bca78fca461008fc9daa4f5ace9819bca7b820
Ruben Costa <daggosso2@gmail.com>
2019-07-08 23:58:15 +0200

    Added styles to paletteList component

--- src/components/MiniPalette/index.js
+++ src/components/MiniPalette/index.js
@@ -35,7 +35,7 @@ export default function MiniPalette(props) {
     return (
         <Container>
             <div>
-                <h1>.</h1>
+                <h1 />
             </div>
             <Title>
                 {paletteName}
--- src/pages/PaletteList/index.js
+++ src/pages/PaletteList/index.js
@@ -2,19 +2,47 @@ import React, { Component } from "react";
 import Navbar from "components/Navbar";
 import { Link } from "react-router-dom";
 import MiniPalette from "components/MiniPalette";
+import styled from "styled-components/macro";
+
+const StyledPaleteList = styled.div`
+    height: 100%;
+    display: flex;
+    align-items: flex-start;
+    justify-content: center;
+`;
+
+const Container = styled.div`
+    width: 50%;
+    display: flex;
+    align-items: flex-start;
+    flex-direction: column;
+    flex-wrap: wrap;
+`;
+
+const Nav = styled.div`
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+    color: white;
+`;
+
+const Palettes = styled.div`
+    box-sizing: border-box;
+    width: 100%;
+    display: grid;
+    grid-template-columns: repeat(3, 30%);
+    grid-gap: 5%;
+`;
 
 export default class PaleteList extends Component {
     renderList() {
         return this.props.palettes.map(palette => {
             return (
-                <div>
-                    <div>
+                <Palettes>
+                    <Link to={`/palettes/${palette.id}`}>
                         <MiniPalette {...palette} />
-                        <Link to={`/palettes/${palette.id}`}>
-                            {palette.paletteName}
-                        </Link>
-                    </div>
-                </div>
+                    </Link>
+                </Palettes>
             );
         });
     }
@@ -23,19 +51,15 @@ export default class PaleteList extends Component {
         return (
             <>
                 <Navbar />
-                <div
-                    style={{
-                        display: "flex",
-                        flexDirection: "column",
-                        color: "white"
-                    }}
-                    className="Home"
-                >
-                    <div style={{ color: "white" }} className="PaletteList">
-                        <h1>PaleteList</h1>
+                <StyledPaleteList>
+                    <Container>
+                        <Nav>
+                            <h1>Color Section</h1>
+                        </Nav>
+
                         {this.renderList()}
-                    </div>
-                </div>
+                    </Container>
+                </StyledPaleteList>
             </>
         );
     }
